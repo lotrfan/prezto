a3a7f1252deeaa9f9b601317b6297446d6212d89
Jeffrey Tolar <tolar.jeffrey@gmail.com>
2014-07-16 19:23:56 -0500

    Add zaw module

---
+++ modules/zaw/init.zsh
@@ -0,0 +1,24 @@
+#
+# Configuration for zaw
+#
+
+
+zstyle ':filter-select:highlight' matched fg=yellow,standout
+zstyle ':filter-select' max-lines 10         # use 10 lines for filter-select
+#zstyle ':filter-select' max-lines -10        # use $LINES - 10 for filter-select
+zstyle ':filter-select' rotate-list yes      # enable rotation for filter-select
+zstyle ':filter-select' case-insensitive yes # enable case-insensitive search
+
+#extended-search:
+#    If this style set to be true value, the searching bahavior will be
+#    extended as follows:
+#
+#    ^ Match the beginning of the line if the word begins with ^
+#    $ Match the end of the line if the word ends with $
+#    ! Match anything except the word following it if the word begins with !
+#    so-called smartcase searching
+#
+#    If you want to search these metacharacters, please doubly escape them.
+zstyle ':filter-select' extended-search yes
+
+source "${0:h}/external/zaw.zsh"
